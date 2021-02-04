614c8fba8440c543fcc4890eb2b4690a6d23ce00
yang-bochen <974491089@qq.com>
2021-02-04 14:24:18 +0800

    第二次修改

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,115 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <Header></Header> -->
+    <!-- enter-active-class="animated bounceInRight"
+      leave-active-class="animated bounceOutLeft" -->
+    <!--   <router-link
+      :to="{ name: 'login' }"
+      v-if="!$route.meta.isShow"
+      @click.native="removeuser"
+      >退出登录</router-link
+    > -->
+    <!-- 在 link中 添加点击事件的时候 需要加.native -->
+    <bottomNav v-if="!$route.meta.isShow"></bottomNav>
+    <!--  <transition
+      mode="out-in"
+      name="custom-classes-transition"
+      enter-active-class="animated bounceInRight"
+      leave-active-class="animated bounceOutLeft"
+    > -->
+    <router-view></router-view>
+    <!-- </transition> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//
+//底部自由导航组件
+import bottomNav from "./components/footer/bottomnav";
+import router from "./router/index";
+// import Header from './components/header/Herder.vue';
+// import { Button } from 'vant'
 
 export default {
-  name: 'App',
+  /*  data() {
+    return {
+      isShow: true,
+    };
+  }, */
+  methods: {
+    removeuser() {
+      localStorage.removeItem("username");
+      this.$router.push("/login");
+    },
+  },
+  watch: {
+    $route() {
+      console.log(this.$route);
+      this.isShow = this.$route.meta.isShow;
+    },
+  },
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    bottomNav,
+  },
+  router,
+};
 </script>
 
 <style>
+.icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+* {
+  background-repeat: no-repeat;
+  padding: 0;
+  margin: 0;
+}
+*,
+:after,
+:before {
+  box-sizing: inherit;
+}
+user agent stylesheet div {
+  display: block;
+}
+.app-shell .app-view-wrapper .app-view[data-v-bc186c1a] {
+  background: #fff;
+  color: #3c3c3c;
+}
+
+#app,
+body,
+html {
+  font-size: 52.1111px;
+}
+* {
+  margin: 0;
+  padding: 0;
+}
+.v-enter,
+.v-leave {
+  transform: translateX(100%);
+}
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.8s;
+}
+/* .bounceInRight,.bounceOutLeft{
+  transition:all 0.8s;
+} */
+/* :root {
+  --animate-duration: 100ms;
+  --animate-delay: 0.1s;
+} */
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+}
+body {
+  height: 100%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import './assets/font_k2eh6lzy7v/iconfont.css'
+import '../public/font_ff7wloml1g7/iconfont.css'
+import '../public/font_ff7wloml1g7/iconfont'
+// import Header from './components/header/Herder.vue'
+// import '@/styles/index.scss'
 Vue.config.productionTip = false
 
+import {
+  Toast,
+  Loading
+} from 'vant';
+
+import store from './store'
+
+Vue.use(Toast);
+Vue.use(Loading);
+
 new Vue({
-  render: h => h(App),
+  store,
+  render: h => h(App)
+  /* components: {
+    Header
+  } */
 }).$mount('#app')
